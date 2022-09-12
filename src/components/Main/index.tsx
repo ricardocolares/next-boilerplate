1e4fd384af197c97168a986d93a096d3a29698ea
Ricardo Colares <ricardo.colares@hp.com>
2022-09-12 09:09:06 -0300

    style: configure styles on the main page with tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>react avancado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo e react avancado escrito ao lado"
+    />
+    <S.Title>React Avancado</S.Title>
+    <S.Description>Typescript, React, Next e Styled Componest</S.Description>
+    <S.Illustatrion
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com codigo"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
