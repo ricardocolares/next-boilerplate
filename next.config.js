442b6253059b79843ea9114be743bf76bd7854b4
Ricardo Colares <ricardo.colares@hp.com>
2023-01-02 16:37:51 -0300

    pwa configured

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  pwa: {
+    disable: !isProd,
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avancado boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512" />
+        <link rel="apple-touch-icon" href="/img/icon-512" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project started to work with ts, next, react and next"
