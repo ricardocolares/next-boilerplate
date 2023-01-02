a56954b9066f893ea65c507b26de875aec611b8c
Ricardo Colares <ricardo.colares@hp.com>
2023-01-02 16:20:51 -0300

    add arg prop to storybook component

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,6 +4,16 @@ import Main from '.';
 export default {
   title: 'Main',
   component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default',
+  },
 } as Meta;
 
 export const Basic: Story = (args) => <Main {...args} />;
+Basic.args = {
+  title: 'title basic 2',
+  description: 'description basic 2',
+};
+
+export const Default: Story = (args) => <Main {...args} />;
